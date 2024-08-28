65dc37d159be39a0239dd57a13662e5f7086a7f9
JuanDiego255 <jastorgalopez6@gmail.com>
2024-08-28 09:59:45 -0600

    Remove storage folder from git

--- storage/framework/views/521bacbff78339fda57d81bd68fbdc78e7ee8c8b.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-    <div class="col-md-12">
-        <?php $__env->startComponent('components.widget', ['title' => __('Rubros Fijos')]); ?>
-        <?php echo $__env->renderComponent(); ?>
-    </div>
-</div>
-<?php /**PATH C:\xampp\htdocs\StoreWeb\resources\views/admin/employees/rubros.blade.php ENDPATH**/ ?>
